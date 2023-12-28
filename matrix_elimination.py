5ca6b219d59968befad3c405dc13b51f4a876d67
Duy Nguyen <yuuwe@Duys-MacBook-Pro.local>
2023-12-28 12:53:17 -0800

    back_sub

--- matrix_elimination.py
+++ matrix_elimination.py
@@ -9,6 +9,7 @@ def sm(size : int, i, j, c) -> np.array:
 	return E
 
 A = np.array([[1,0,0,0],[2,1,0,0],[3,5,1,1],[1,3,5,1]])
+x = [5,10,3,1]
 
 def elimination(A : np.array) -> tuple[np.array, []]:
 	if A.shape[0] != A.shape[1]:
@@ -45,14 +46,38 @@ def elimination(A : np.array) -> tuple[np.array, []]:
 	return(U, E)
 	
 U, E = elimination(A)
+
+y = np.dot(E,x)
+
 print(U)
+print(y)
 
+x3 = y[3]/U[3,3]
+x2 = (y[2]-U[2,3]*x3)/U[2,2]
+x1 = (y[1]-U[1,2]*x2-U[1,3]*x3)/U[1,1]
+x0 = (y[0]-U[0,1]*x1-U[0,2]*x2-U[0,3]*x3)/U[0,0]
 
-def backward_sub(U : np.array,y : np.array):
-	print(U.shape[0])
-	print(U)	
+x = np.array([x0,x1,x2,x3])
 
-#backward_sub(U,y)
+print(x)
+
+def backward_sub(U : np.array, y : np.array):
+	n = U.shape[0]
+
+	X = []	
+	for i in range(0,n)[::-1]:
+	#	print("i",i)
+		x = y[i]
+		for k in range(i+1,n):
+	#		print("k",k)
+			x = x - U[i,k]*X[k-n]
+		x = x / U[i,i]
+		X.append(x)
+
+	# reverses list at the end
+	X = X[::-1]
+
+backward_sub(U,y)
 
 # print original equation
 def p_a():
